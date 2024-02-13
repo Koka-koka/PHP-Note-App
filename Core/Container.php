fb5eb35a4a794420c29a086524ce43fbb8d9ebff
Koka M <kokamerabishvili@gmail.com>
2024-02-14 03:36:25 +0400

    create service container, add Container and App class, create bootstrap.php for db connection

---
+++ Core/Container.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core;
+
+class Container
+{
+	protected $bindings = [];
+
+	public function bind($key, $resolver)
+	{
+		$this->bindings[$key] = $resolver;
+	}	
+
+	public function resolve($key)
+	{
+
+		if (!array_key_exists($key, $this->bindings)) {
+			throw new \Exception("No matching binding found for {$key}", 1);
+		}
+		
+		$resolver = $this->bindings[$key];
+
+		return call_user_func($resolver); 
+		
+	}
+}
--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -2,9 +2,9 @@
 
 use Core\Database;
 use Core\Validator;
+use Core\App;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
 
 $errors = [];
 
