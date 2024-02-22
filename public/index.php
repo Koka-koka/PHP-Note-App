3bc4f570f55d22b2c155104aeeab1074b901e823
Koka M <kokamerabishvili@gmail.com>
2024-02-23 02:31:03 +0400

    implement composer autoloading.

--- public/index.php
+++ public/index.php
@@ -6,14 +6,9 @@ define("BASE_PATH", __DIR__ . '/../');
 
 define("ROOT_URL", '/' . explode('/', $_SERVER['REQUEST_URI'])[1] . '/');
 
+require BASE_PATH . 'vendor/autoload.php';
+
 require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-
-	$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-	require base_path("{$class}.php");
-});
 
 require base_path("bootstrap.php");
 
